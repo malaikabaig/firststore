279705d474b6b775e86524d28bf36d56f4fea25f
malaikabaig <malaikabaig1@gmail.com>
2024-08-03 19:55:25 +0500

    commit

--- pages/cart.js
+++ pages/cart.js
@@ -5,8 +5,9 @@ import { XCircleIcon } from '@heroicons/react/outline';
 import Link from 'next/link';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
+import dynamic from 'next/dynamic';
 
-export default function CartScreen() {
+function CartScreen() {
   const { state, dispatch } = useContext(Store);
   const {
     cart: { cartItems },
@@ -31,44 +32,43 @@ export default function CartScreen() {
         <div className="grid md:grid-cols-4 md:gap-5">
           <div className="overflow-x-auto md:col-span-3">
             <table className="min-w-full">
-              <thead className="border-b"></thead>
-              <tr>
-                <th className="px-5 text-left">Item</th>
-                <th className="p-5 text-right">Quantity</th>
-                <th className="p-5 text-right">Price</th>
-                <th className="p-5">Action</th>
-              </tr>
+              <thead className="border-b">
+                <tr>
+                  <th className="px-5 text-left">Item</th>
+                  <th className="p-5 text-right">Quantity</th>
+                  <th className="p-5 text-right">Price</th>
+                  <th className="p-5">Action</th>
+                </tr>
+              </thead>
               <tbody>
                 {cartItems.map((item) => (
                   <tr key={item.slug} className="border-b">
                     <td>
-                      <Link href={`/product/${item.slug}`}>
-                        <Image
-                          src={item.image}
-                          alt={item.name}
-                          width={50}
-                          height={50}
-                        ></Image>
-                        &nbsp;
-                        {item.name}
+                      <Link href={`/product/${item.slug}`} legacyBehavior>
+                        <a className="flex items-center">
+                          <Image
+                            src={item.image}
+                            alt={item.name}
+                            width={50}
+                            height={50}
+                          ></Image>
+                          &nbsp;
+                          {item.name}
+                        </a>
                       </Link>
                     </td>
                     <td className="p-5 text-right">
                       <select
-                        value={item.qunatity}
+                        value={item.quantity}
                         onChange={(e) =>
                           updateCartHandler(item, e.target.value)
                         }
                       >
-                        {[
-                          ...Array(item.countInStock)
-                            .keys()
-                            .map((x) => (
-                              <option key={x + 1} value={x + 1}>
-                                {x + 1}
-                              </option>
-                            )),
-                        ]}
+                        {[...Array(item.countInStock).keys()].map((x) => (
+                          <option key={x + 1} value={x + 1}>
+                            {x + 1}
+                          </option>
+                        ))}
                       </select>
                     </td>
                     <td className="p-5 text-right">${item.price}</td>
@@ -105,3 +105,5 @@ export default function CartScreen() {
     </Layout>
   );
 }
+
+export default dynamic(() => Promise.resolve(CartScreen), { ssr: false });
